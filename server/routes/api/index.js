afdd931fd4001fcaf1ea40124fff58a86e1b9030
Arnaldo Sepulveda <arnaldosepulveda@gmail.com>
2024-04-05 19:46:09 -0300

    add ai-route to /api/index

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,8 +1,14 @@
 const router = require('express').Router();
 const userRoutes = require('./user-routes');
+// Import the AI routes
+const aiRoutes = require('./ai-routes');
 
 // User Routes
 // All requests to `/api/users` are forwarded to `userRoutes` for handling
 router.use('/users', userRoutes);
+
+// AI Routes
+// All requests to `/api/ai` are forwarded to `aiRoutes` for handling
+router.use('/ai', aiRoutes);
 
 module.exports = router;
