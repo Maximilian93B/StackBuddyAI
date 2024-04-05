66fc7f176b7cfdc5e4d1f67dd21dbfd0a2ccbf35
Maximilian93B <147316673+Maximilian93B@users.noreply.github.com>
2024-04-05 00:12:25 -0300

    added typeDefs for Project/User/techCategory + Queries & mutations

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,12 +1,88 @@
- // TypeDefs file 
+const { gql } = require('apollo-server-express');
 
- const { gql } = require('apollo-server-express');
- const typeDefs = gql`
- type Query {
-    hello: String
- }
- `;
+/* Types 
+Project
+User
+TechCategory
 
- module.exports = typeDefs;
+These types will define our main data. 
+! = Non Nullable = Must return values 
 
- 
+input type =  creating and updating projects 
+
+CRUD OPS - Will need to elaborate 
+
+TO DO : 
+
+Error Handling ? 
+
+
+*/
+
+const typeDefs = gql`
+   
+#       Define Project type
+
+type Project {
+    id: ID!
+    title: String!
+    description: String!
+    useQueries: [String!] # User queries for tech stack recommendation
+    techSelection: [TechCategory] # Tech categories and selected technologies
+    comments: [String!] # User comments on the project
+    dateStamp: String! 
+    owner: User! # The user who owns the project --> Will use for permissions for project
+}
+
+#           User type
+
+type User {
+    id: ID!
+    username: String!
+    email: String!
+    projects: [Project!]! # Projects owned by the user
+}
+
+
+#       Define a Tech Category 
+
+type TechCategory {
+    categpry: String!
+    technologies: [String!] # Technologies selected for each category
+}
+
+
+#            Queries
+type Query {
+    projects: [Project!]! # Fetch all projects
+    project(id: ID! ): Project # Fetch a single project by ID 
+    users: [User!] # Fetch all users
+    user(id: ID): User # Fetch a single user by ID 
+}
+
+
+
+#           Mutations 
+
+type Mutation {
+    createProject(title: String!, description: String!, useQueries: [String], techSelection: [TechCategoryInput], comments: [String]): Project
+    updateProject(id: ID!, title: String, description: String, userQueries: [String], techSelection: [TechCategoryInput], comments: [String]): Project
+}
+
+ #          Inpurt type for tech selection when creating/updating projects
+
+ input TechCategoryInput {
+    category: String!
+    technologies: [String]!
+  }
+
+
+#           Auth payload for login/signup mutations 
+
+type AuthPayload {
+    token: String! # Authentication token
+    user: User! # Logged in user information
+}
+`;
+
+module.exports = typeDefs;
