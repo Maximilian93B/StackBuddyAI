b4d5f030212faeb781ffac1eb62a5be92e88f999
Maximilian93B <147316673+Maximilian93B@users.noreply.github.com>
2024-04-08 23:09:49 -0300

    added dashboard to workstation

--- client/src/pages/Workstation.jsx
+++ client/src/pages/Workstation.jsx
@@ -1,21 +1,37 @@
 import React, { useEffect } from 'react'
-import CreateProjectForm from '../components/CreateProjectForm';
 import AuthService from '../utils/auth';
 import { useNavigate } from 'react-router-dom';
-function Workstation () {
+import Dashboard from '../components/Dashboard';
+import styled from 'styled-components';
+
+const WorkstationContainer = styled.div`
+  display: flex;
+  min-height: 100vh; // Full height of the viewport
+`;
+
+const ContentContainer = styled.div`
+  flex-grow: 1; // Takes up the remaining space
+  padding: 20px; // Add padding for some spacing around your content
+`;
+
+
+function Workstation() {
     const navigate = useNavigate();
 
     useEffect(() => {
         if (!AuthService.loggedIn()) {
-            navigate('/login'); // Adjust the path as needed
+            navigate('/login');
         }
     }, [navigate]);
+    
     return (
-        <div>
-            <CreateProjectForm></CreateProjectForm>
-            <h1>This is out workstation page</h1>
-        </div>
-    )
+      <WorkstationContainer>
+        <Dashboard />
+        <ContentContainer>
+            <h1>This is our workstation page</h1>
+        </ContentContainer>
+      </WorkstationContainer>
+    );
 }
 
-export default Workstation; 
+export default Workstation;
