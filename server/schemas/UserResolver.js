b4c1a89bbc6c850e818f0777d82827ffa746ea97
Arnaldo Sepulveda <arnaldosepulveda@gmail.com>
2024-04-09 09:54:21 -0300

    tyoeDefs and Resolver

--- server/schemas/UserResolver.js
+++ server/schemas/UserResolver.js
@@ -10,21 +10,22 @@ const userResolvers = {
       if (!context.user) {
         throw new AuthenticationError('Not Logged in');
       }
-        return await User.findById(context.user._id);
+        return await User.findById(context.user._id).populate('currentProjects');
     },
 
     // Query to fetch all users 
     users: async () => {
-      return await User.find({});
+      return await User.find({}).populate('currentProjects');
     },
 
     // Query to fetch a single user 
     user: async (_, { id }) => {
-      return await User.findById(id);
+      return await User.findById(id).populate('currentProjects');
     },    
     // Add querys as we need them 
   },
   Mutation: {
+
     // Sign up a new user and return  auth token and  user details
     signup: async (_, { username, email, password }) => {
       const user = await User.create({ username, email, password });
@@ -32,6 +33,7 @@ const userResolvers = {
       return { token, user };
 
     },
+
     // Login and return auth token 
     login: async (_, {email, password }) => {
       const user = await User.findOne({ email });
@@ -47,11 +49,14 @@ const userResolvers = {
     // Will need to include more mutations to handle users updating more than email 
     // Need to handle permissions for users still 
     
-  }
+  },
 
-
-
-
+  User: {
+    projectCount: (parent) => {
+      return parent.currentProjects?.length || 0;
+    },
+  },
+  
 };
 
 module.exports = userResolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -21,7 +21,18 @@ Error Handling ?
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-   
+
+#       Define User type
+
+type User {
+    id: ID!
+    username: String!
+    email: String!
+    currentProjects: [Project!] # Projects owned by the user
+    projectCount: Int
+}
+
+
 #       Define Project type
 
 type Project {
@@ -35,30 +46,36 @@ type Project {
     owner: User! # The user who owns the project --> Will use for permissions for project
 }
 
+#       Define a Tech Category type
 
-#           User type
-
-type User {
-    id: ID!
-    username: String!
-    email: String!
-    projects: [Project!]! # Projects owned by the user
+type TechCategory {
+    category: String!
+    technologies: [Technology!] # Technologies selected for each category
 }
 
+#       Define Technology type
+
+  type Technology {
+    id: ID!
+    name: String!
+    category: String!
+    description: String # If we have a description field
+    comments: [Comment] # each technology can have multiple comments
+  }
+
+#       Define a Comment 
+
+type Comment {
+    id: ID!
+    text: String!
+    postedBy: User!
+    dateStamp: String!
+}
 
 type AuthPayload {
     token: String!
     user: User
 }
-
-
-#       Define a Tech Category 
-
-type TechCategory {
-    category: String!
-    technologies: [String!] # Technologies selected for each category
-}
-
 
 type Query {
     projects: [Project!]!
@@ -66,37 +83,70 @@ type Query {
     users: [User!]
     user(id: ID!): User
     me: User
+    technologies: [Technology]
+    technology(id: ID!): Technology
 }
 
 #           Mutations 
 
 type Mutation {
-    signup(username: String!, email: String!, password: String!): AuthPayload
-    login(email: String!, password: String!): AuthPayload
+    signup(
+        username: String!, 
+        email: String!, 
+        password: String!
+    ): AuthPayload
+    login(
+        email: String!, 
+        password: String!
+    ): AuthPayload
     createProject(
-      title: String!,
-      description: String!,
-      userQueries: [String], # Made optional
-      techSelection: [TechCategoryInput!], # require at least one TechCategoryInput
-      comments: [String] # Made optional
+        title: String!,
+        description: String!,
+        userQueries: [String], # Made optional
+        techSelection: [TechCategoryInput!], # require at least one TechCategoryInput
+        comments: [String] # Made optional
     ): Project
+    createTechnology(
+        name: String!, 
+        category: String!, 
+        comments: [CommentInput]
+    ): Technology
     updateProject(
-      id: ID!,
-      title: String,
-      description: String,
-      userQueries: [String], # optional as well
-      techSelection: [TechCategoryInput], #  updating might not require changing tech selection
-      comments: [String] #  optional 
+        id: ID!,
+        title: String,
+        description: String,
+        userQueries: [String], # optional as well
+        techSelection: [TechCategoryInput], #  updating might not require changing tech selection
+        comments: [String] #  optional 
     ): Project
-  }
+    updateTechnology(
+        id: ID!, 
+        name: String, 
+        category: String, 
+        description: String, 
+        comments: [String]
+    ): Technology
+    deleteProject(
+        id: ID!
+    ): Project
+    deleteTechnology(
+        id: ID!
+    ): Technology
+}
   
 
  #          Inpurt type for tech selection when creating/updating projects
 
- input TechCategoryInput {
-    category: String!
-    technologies: [String!]!
-}
+    input TechCategoryInput {
+        category: String!
+        technologies: [String!]!
+    }
+
+    input CommentInput {
+        text: String
+        postedBy: ID
+        dateStamp: String
+    }
 
 `;
 
